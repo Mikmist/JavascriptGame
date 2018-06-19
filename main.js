5f5bd835361e5ae9291cdc1dbf1de050725960d7
Zino Holwerda <z.holwerda@ho2o.nl>
2018-06-19 17:44:32 +0200

    Added notify function

--- main.js
+++ main.js
@@ -5,52 +5,64 @@ var food = false;
 var wood = false;
 var array = []
 
-
 window.setInterval(function(){
-  startFunction();
-  console.log(food);
-  console.log(wood);
-  woodFunction();
-  foodFunction();
-  document.getElementById('foodDisplay').innerHTML = "Food: " + round(foodNumber);
-  document.getElementById('woodDisplay').innerHTML = "Wood: " + round(woodNumber);
-},1000)
+    console.log(food);
+    console.log(wood);
+    woodFunction();
+    foodFunction();
+    document.getElementById('foodDisplay').innerHTML = "Food: " + round(foodNumber);
+    document.getElementById('woodDisplay').innerHTML = "Wood: " + round(woodNumber);
+}, 1000)
 
 function round(input){
-  return Math.round(input * 100) / 100;
+    return Math.round(input * 100) / 100;
 }
 
 function foodFunction(){
-  foodNumber -= 0.2*population;
-  if (food){
-    foodNumber += 1;
-  }
+    foodNumber -= 0.2*population;
+    if (food){
+      foodNumber += 1;
+    }
 }
 
 function woodFunction(){
-  if (wood){
-    woodNumber += 1;
-  }
+    if (wood){
+      woodNumber += 1;
+    }
 }
 
 function foodButton(){
-  if(food){
-    food = false;
-  } else {
-    food = true;
-    wood = false;
-  }
+    if(food){
+      food = false;
+    } else {
+      food = true;
+        wood = false;
+    }
 }
 
 function woodButton(){
-  if(wood){
-    wood = false;
-  } else {
-    wood = true;
-    food = false;
-  }
+    if(wood){
+        wood = false;
+    } else {
+        wood = true;
+        food = false;
+    }
 }
 
-function notifications(array) {
-    document.getElementById("textDisplay").innerHTML = array;
+/*
+ * This function displays notifications with a max of 5.
+ */
+function notify(notification) {
+    var el = document.getElementById('textDisplay');
+    var array = el.innerHTML.split("<br>");
+    var i;
+
+    el.innerHTML = "";
+
+    var length = Math.min(5, array.length);
+    for(i = 1; i < length; i++) {
+        el.innerHTML += array[i] + "<br>";
+    }
+
+    el.innerHTML += notification + "<br>";
 }
